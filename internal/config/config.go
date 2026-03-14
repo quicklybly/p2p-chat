9255046a352bcd4901838b2000c19f354a5f4bc0
ar.r.lysenko <ar.r.lysenko@tbank.ru>
2026-03-14 16:07:28 +0300

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import "time"
+
+type Config struct {
+	P2P P2PConfig
+}
+
+type P2PConfig struct {
+	ListenAddrs       []string
+	BootstrapPeers    []string
+	EnableMDNS        bool
+	ReProvideInterval time.Duration
+}
+
+func Load() Config {
+	return Config{
+		P2P: P2PConfig{
+			ListenAddrs: []string{
+				"/ip4/0.0.0.0/tcp/0",
+				"/ip4/0.0.0.0/udp/0/quic-v1",
+			},
+			BootstrapPeers:    []string{},
+			EnableMDNS:        true,
+			ReProvideInterval: 10 * time.Minute,
+		},
+	}
+}
