a526d6119954190263bedeba7621d6abe3f25ba8
ar.r.lysenko <ar.r.lysenko@tbank.ru>
2026-03-14 15:48:11 +0300

    init project

---
+++ cmd/chat/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("P2P Chat v0.1")
+	fmt.Println("Node starting...")
+	fmt.Println("Ready.")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/quicklybly/p2p-chat
+
+go 1.25.6
